dadb335177e6550e369d02145fddbf6bf02e36ec
Ville Halldin <villehalldin05@gmail.com>
2023-09-26 01:25:05 +0200

    Added enemies with a stateEngine

---
+++ FPSGame/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private StateMachine stateMachine;
+    private NavMeshAgent agent;
+    public NavMeshAgent Agent { get => agent; }
+    public Path path;
+
+    [SerializeField] // for debugging
+    private string currentState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stateMachine = GetComponent<StateMachine>();
+        agent = GetComponent<NavMeshAgent>();
+        stateMachine.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FPSGame/Assets/Scripts/Enemy/StateMachine.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    public BaseState activeState;
+    public PatrolState patrolState;
+
+    public void Initialize()
+    {
+        patrolState = new PatrolState();
+        ChangeState(patrolState);
+    }
+
+    private void Update()
+    {
+        if (activeState != null)
+        {
+            activeState.Perform();
+        }
+    }
+
+    public void ChangeState(BaseState newState)
+    {
+        if (activeState != null)
+        {
+            activeState.Exit();
+        }
+
+        activeState = newState;
+
+        if (activeState != null)
+        {
+            activeState.stateMachine = this;
+            activeState.enemy = GetComponent<Enemy>();
+            activeState.Enter();
+        }
+    }
+}
